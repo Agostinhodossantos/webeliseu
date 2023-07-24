b22677954e4ba8e8e374ce9fb13d4832f801ed41
Agostinhodossantos <agostinhodossantos27@gmail.com>
2023-07-24 15:13:12 +0200

    update title

--- src/views/examples/LoginPageMain.js
+++ src/views/examples/LoginPageMain.js
@@ -120,7 +120,11 @@ export default function LoginPageMain() {
                                 {/*</h4>*/}
 
                                 <div className="btn-wrapper">
-                                    <img alt="..." src={require("assets/img/Descobrindo.png")} />
+                                    {/*<img alt="..." src={require("assets/img/Descobrindo.png")} />*/}
+                                    <iframe width="100%" height="415" src="https://www.youtube.com/embed/vMtVRxvlvys"
+                                            title="YouTube video player" frameBorder="0"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                            allowFullScreen></iframe>
                                 </div>
                             </Col>
 
